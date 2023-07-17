d78f8574f0147aa7c3f0a902b0d7dfd2f2623d18
Tactchou marc <marcanalytica@gmail.com>
2023-07-17 14:31:31 +0100

    redirect v3

--- src/Controller/XbetController.php
+++ src/Controller/XbetController.php
@@ -101,7 +101,7 @@ class XbetController extends AbstractController
     
             $mailer->send($v_email);
 
-            return $this->redirect('https://wa.me/2250708618478?id_compte=$id_compte&montant=$montant&code_recu=$code_recu&numero_recu=$numero_recu&pays=$pays');
+            return $this->redirect('https://wa.me/2250708618478/?text=id_compte%3D%24id_compte%26montant%3D%24montant%26id_transaction%3D%24id_transaction%26numero_paiement%3D%24numero_paiement%26pays%3D%24pays');
         }
 
         return $this->render('depot.html.twig', compact('msg'));
@@ -197,7 +197,7 @@ class XbetController extends AbstractController
     
             $mailer->send($v_email);
 
-            return $this->redirect('https://wa.me/2250708618478?id_compte=$id_compte&montant=$montant&code_recu=$code_recu&numero_recu=$numero_recu&pays=$pays');
+            return $this->redirect('https://wa.me/2250708618478?text=id_compte%3D%24id_compte%26montant%3D%24montant%26code_recu%3D%24code_recu%26numero_recu%3D%24numero_recu%26pays%3D%24pays');
         }
 
         return $this->render('retrait.html.twig', compact('msg'));
