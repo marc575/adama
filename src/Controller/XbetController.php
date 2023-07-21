ea9215299fa06906541e87838b79b4e94ddbd1ff
Tactchou marc <marcanalytica@gmail.com>
2023-07-21 12:13:22 +0100

    whatsapp v5

--- src/Controller/XbetController.php
+++ src/Controller/XbetController.php
@@ -99,7 +99,7 @@ class XbetController extends AbstractController
     
             $mailer->send($v_email);
 
-            return $this->redirect('https://wa.me/2250708618478');
+            return $this->redirect('https://wa.me/2250708618478?text=');
         }
 
         return $this->render('depot.html.twig', compact('msg'));
@@ -107,95 +107,8 @@ class XbetController extends AbstractController
     }
 
     #[Route('/xbet/retrait', name: 'retrait')]
-    public function retrait(Request $request, ManagerRegistry $doctrine, MailerInterface $mailer) : Response
+    public function retrait() : Response
     {
-        $msg = null;
-        if($request->isMethod(Request::METHOD_POST)) 
-        {
-            $email = $request->request->get('email');
-            $id_compte = $request->request->get('id_compte');
-            $montant = $request->request->get('montant');
-            $code_recu = $request->request->get('code_recu');
-            $numero_recu = $request->request->get('numero_recu');
-            $cnumero_recu = $request->request->get('cnumero_recu');
-            $pays = $request->request->get('pays');
-
-            if(!preg_match("#.{2,}@[a-zA-Z]{2,}\.[a-zA-Z]{2,5}#", $email))
-            {
-                $msg = "Ajoutez une adresse mail valide";
-
-                return $this->render('retrait.html.twig', compact('msg'));
-            }
-            if(!$id_compte || strlen($id_compte) < 8)
-            {
-                $msg = "Precisez l'identifiant exacte de votre compte 1xbet";
-
-                return $this->render('retrait.html.twig', compact('msg'));
-            }
-            if(!$numero_recu || strlen($numero_recu) < 8)
-            {
-                $msg = "Renseignez le numéro de reçu exacte";
-
-                return $this->render('retrait.html.twig', compact('msg'));
-            }
-            if($montant < 3000)
-            {
-                $msg = "Le montant minimal est de 3000 XOF";
-
-                return $this->render('retrait.html.twig', compact('msg'));
-            }
-            if(!$cnumero_recu || $cnumero_recu != $numero_recu)
-            {
-                $msg = "Vérifier bien la confirmation de votre numero de reçu";
-
-                return $this->render('retrait.html.twig', compact('msg'));
-            }
-
-            $retrait = new Retrait();
-            $retrait->setEmail($email);
-            $retrait->setIdCompte($id_compte);
-            $retrait->setMontant($montant);
-            $retrait->setCodeRecu($code_recu);
-            $retrait->setNumeroRecu($cnumero_recu);
-            $retrait->setCnumeroRecu($numero_recu);
-            $retrait->setPays($pays);
-            $retrait->setDate(new \DateTime('now'));
-
-            $em = $doctrine->getManager();
-            $em->persist($retrait);
-            $em->flush();
-
-            $c_email = (new TemplatedEmail())
-                ->to($email)
-                ->subject('BS Exchange Services | Bon de commande de votre retrait 1XBET')
-                ->htmlTemplate('email/retrait.html.twig')
-                ->context([
-                    'id_compte' => $id_compte,
-                    'montant' => $montant,
-                    'code_recu' => $code_recu,
-                    'numero_recu' => $numero_recu,
-                    'pays' => $pays,
-                ]);
-    
-            $mailer->send($c_email);
-
-            $v_email = (new TemplatedEmail())
-                ->to('barryadamagd@gmail.com')
-                ->subject('BS Exchange Services | Bon de commande pour un nouveau retrait 1XBET')
-                ->htmlTemplate('email/retrait.html.twig')
-                ->context([
-                    'id_compte' => $id_compte,
-                    'montant' => $montant,
-                    'code_recu' => $code_recu,
-                    'numero_recu' => $numero_recu,
-                    'pays' => $pays,
-                ]);
-    
-            $mailer->send($v_email);
-
-            //return $this->redirect('https://wa.me/2250708618478');
-        }
-
-        return $this->render('retrait.html.twig', compact('msg'));
+        return $this->render('retrait.html.twig');
     }
 }
