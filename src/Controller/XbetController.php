9eabf9b394280cd6d1b8f911fb512beed3baa2e6
Tactchou marc <marcanalytica@gmail.com>
2023-09-04 10:40:56 +0100

    API v6

--- src/Controller/XbetController.php
+++ src/Controller/XbetController.php
@@ -101,7 +101,7 @@ class XbetController extends AbstractController
     
             $mailer->send($v_email);
 
-            return $this->redirect('https://api.whatsapp.com/send?phone=2250708618478&amp; text=Id_compte=$id_compte%0Montant=$montant%0Id_transaction=$id_transaction%0Numero_paiement=$numero_paiement%0Pays=$pays');
+            return $this->redirect('https://api.whatsapp.com/send?phone=2250708618478&amp;text=Id_compte=$id_compte%0Montant=$montant%0Id_transaction=$id_transaction%0Numero_paiement=$numero_paiement%0Pays=$pays');
         }
 
         return $this->render('depot.html.twig', compact('msg'));
